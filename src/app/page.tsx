753285d389c3fc1399870b7c26fe90c8d3636e18
TeeBo8 <t.leture@gmail.com>
2025-04-10 13:27:02 +0200

    Fix: Renommer le dossier (marketing) en marketing pour résoudre les problèmes de déploiement Vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 // Plus besoin de redirection qui crée une boucle infinie
 // On importe et on utilise directement le composant de la page marketing
-import Marketing from "./(marketing)/page";
+import Marketing from "./marketing/page";
 
 export default Marketing;
